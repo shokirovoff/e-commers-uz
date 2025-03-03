ea4d74ed1578b44cdd6efacde41513cef82d0bdb
BaxtiyorCoder <baxtiyorcoder0@gmail.com>
2025-03-03 14:36:39 +0500

    Fix Next.js params type issue

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,45 +2,35 @@ import CustomImage from "@/components/image";
 import { notFound } from "next/navigation";
 
 interface Props {
-  params: {
-    id: string;
-  };
+  params: Record<string, string>;
 }
 
-const ProductDetail = async ({params: { id }}: Props) => {
-  try{
-    const res = await fetch(
-      `https://fakestoreapi.com/products/${id}`
-    );
-    const product = await res.json()
+const ProductDetail = async ({ params: { id } }: Props) => {
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
 
-    return(
-      <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-8 px-4 mt-6 pb-10">
-        <CustomImage product={product} />
+  if (!res.ok) {
+    console.error(`Product not found: ${id}`);
+    return notFound();
+  }
 
-        <div className="divide-2">
-          <div className="space-y-2 pb-8">
-            <h1 className="text-2xl md:text-4xl font-bold">
-              {product.title}
-            </h1>
-            <h2 className="text-gray-500 font-bold text-xl">
-              ${product.price}
-            </h2>
-          </div>
+  const product = await res.json();
 
-          <div className="pt-8">
-            <p className="text-xs md:text-sm">
-              {product.description}
-            </p>
-          </div>
+  return (
+    <div className="max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-8 px-4 mt-6 pb-10">
+      <CustomImage product={product} />
+
+      <div className="divide-2">
+        <div className="space-y-2 pb-8">
+          <h1 className="text-2xl md:text-4xl font-bold">{product.title}</h1>
+          <h2 className="text-gray-500 font-bold text-xl">${product.price}</h2>
+        </div>
+
+        <div className="pt-8">
+          <p className="text-xs md:text-sm">{product.description}</p>
         </div>
       </div>
-    );
-  } catch( error ){
-    notFound();
-    console.log(error)
-  }
-}
+    </div>
+  );
+};
 
-export default ProductDetail
-
+export default ProductDetail;
